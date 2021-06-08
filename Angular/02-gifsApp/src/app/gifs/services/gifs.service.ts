2695c27c8e02f9e1f4a626ce4001dd23074b8da5
Jonathan Roberts <jonathanroberts992@gmail.com>
2021-06-08 00:23:55 -0300

    First endpoint calls with http

--- Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,27 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-
   private apiKey: string = 'NUtLt9srQ8s42A159oiGdQlkl6GmwL88';
   private _historial: string[] = [];
 
-  get historial(){
+  //TODO: Cambiar any por su tipo
+  public resultados: any[] = [];
+
+  get historial() {
     return [...this._historial];
   }
 
-  buscarGifs(query: string = ''){
+  constructor(private http: HttpClient){
 
+  }
+
+  buscarGifs(query: string = '') {
     query = query.trim().toLowerCase();
 
-    if(!this._historial.includes(query)){
+    if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
     }
 
-    console.log(this._historial);
-  }
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=NUtLt9srQ8s42A159oiGdQlkl6GmwL88&q=${query}&limit=10`)
+      .subscribe( (resp: any) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
 
+  }
 }
