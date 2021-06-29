30ddd297a9897c27ab21d4aaef4848a00e145b1a
jonathanroberts992 <jonathanroberts992@gmail.com>
2021-06-29 00:40:50 -0300

    Empiezo x el detalle del país

--- Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params
+      /*subscribe(params => {
+      console.log(params.id);
+    });*/
+      /*subscribe(params => {
+      console.log(params.id);
+    });*/
+      .subscribe(({ id }) => {
+        console.log(id);
 
+        this.paisService.getPaisPorAlphaCode(id)
+        .subscribe(pais => {
+          console.log(pais);
+        });
+      });
+  }
 }
