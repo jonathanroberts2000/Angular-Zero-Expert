0c0de523ae1f1b1de8b9ab31058c8ceb0cb254dc
jonathanroberts992 <jonathanroberts992@gmail.com>
2021-07-13 23:51:22 -0300

    Primeros pasos PrimeNG

--- Angular/04-pipesApp/src/app/app.component.ts
+++ Angular/04-pipesApp/src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'jonAthaN roberTs';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Jonathan'
+  };
+
+  mostrarNombre(): void {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
