7949e48723642c1cf9e033e0dfe9b119d6da870d
jonathanroberts992 <jonathanroberts992@gmail.com>
2021-07-19 00:11:00 -0300

    Optimizacion de modulos y menuBar PrimeNG

--- Angular/04-pipesApp/src/app/app.module.ts
+++ Angular/04-pipesApp/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 //Modulo personalizado
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PrimeNgModule],
+  imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
