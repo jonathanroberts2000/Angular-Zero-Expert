86c4767ea431f8f98db222e00cb63fd2de974b67
jonathanroberts992 <jonathanroberts992@gmail.com>
2021-07-18 23:24:18 -0300

    PrimeNg - modulo personalizado

--- Angular/04-pipesApp/src/app/app.module.ts
+++ Angular/04-pipesApp/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button';
 import { AppComponent } from './app.component';
-import { CardModule } from 'primeng/card';
+
+//Modulo personalizado
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ButtonModule, CardModule],
+  imports: [BrowserModule, PrimeNgModule],
   providers: [],
   bootstrap: [AppComponent],
 })
