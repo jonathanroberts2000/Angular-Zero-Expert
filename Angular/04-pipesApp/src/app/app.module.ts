2ec6ec5dbaeb4ada701203b9186226e7540267fd
jonathanroberts992 <jonathanroberts992@gmail.com>
2021-07-19 23:24:03 -0300

    Pages added + Router added

--- Angular/04-pipesApp/src/app/app.module.ts
+++ Angular/04-pipesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 
 //Modulo personalizado
@@ -7,7 +8,7 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, SharedModule, AppRouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
