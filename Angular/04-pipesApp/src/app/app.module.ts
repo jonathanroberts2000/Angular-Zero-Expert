2c9d1c943402a484636a3cb93cba94d65e5fb799
jonathanroberts992 <jonathanroberts992@gmail.com>
2021-07-26 22:45:39 -0300

    date pipe completed + cambio global a español

--- Angular/04-pipesApp/src/app/app.module.ts
+++ Angular/04-pipesApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
@@ -7,10 +7,17 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
+//Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es-AR';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR' }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
