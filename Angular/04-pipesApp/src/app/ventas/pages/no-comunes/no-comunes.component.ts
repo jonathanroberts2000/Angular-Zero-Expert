4670ca4291569188e483833f66619538fcefb41f
jonathanroberts992 <jonathanroberts992@gmail.com>
2021-07-30 09:32:26 -0300

    + avances en pipes

--- Angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ Angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NoComunesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NoComunesComponent {}
