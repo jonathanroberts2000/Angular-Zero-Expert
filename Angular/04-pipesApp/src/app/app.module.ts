0c91c4b015546af85544780fdff60ef90d32d401
jonathanroberts992 <jonathanroberts992@gmail.com>
2021-07-31 18:58:01 -0300

    mas avances con primeNg

--- Angular/04-pipesApp/src/app/app.module.ts
+++ Angular/04-pipesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 
@@ -16,7 +17,13 @@ registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    AppRouterModule,
+    VentasModule,
+    BrowserAnimationsModule,
+  ],
   providers: [{ provide: LOCALE_ID, useValue: 'es-AR' }],
   bootstrap: [AppComponent],
 })
