0686f281fb744cc8097e3524a5a424807c8bca9e
Jonathan Esteban Roberts <jonathanroberts992@gmail.com>
2021-10-09 00:11:23 -0300

    Angular updated + routerLink + activatedRoute

--- Angular/05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
+++ Angular/05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -8,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe( ({id}) => console.log(id));
   }
 
 }
