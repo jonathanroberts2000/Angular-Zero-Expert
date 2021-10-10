348b0297dc50a324083b0ec09bbbf88a899ba26f
Jonathan Esteban Roberts <jonathanroberts992@gmail.com>
2021-10-10 17:03:19 -0300

    Buscar iniciado - import de modulos

--- Angular/05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
+++ Angular/05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -7,12 +7,14 @@ import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [`
-    img {
-      width: 100%;
-      border-radius: 5px;
-    }
-  `],
+  styles: [
+    `
+      img {
+        width: 100%;
+        border-radius: 5px;
+      }
+    `,
+  ],
 })
 export class HeroeComponent implements OnInit {
   heroe!: Heroe;
@@ -25,6 +27,6 @@ export class HeroeComponent implements OnInit {
   ngOnInit(): void {
     this.route.params
       .pipe(switchMap(({ id }) => this.heroesService.getHeroePorId(id)))
-      .subscribe( heroe => this.heroe = heroe);
+      .subscribe((heroe) => (this.heroe = heroe));
   }
 }
--- Angular/05-heroesApp/src/app/material/material.module.ts
+++ Angular/05-heroesApp/src/app/material/material.module.ts
@@ -8,6 +8,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   exports: [
@@ -19,6 +22,9 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatCardModule,
     MatGridListModule,
     MatProgressSpinnerModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
 })
 export class MaterialModule {}
