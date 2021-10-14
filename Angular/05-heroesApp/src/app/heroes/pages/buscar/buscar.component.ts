9b55bc0de8bbe55b608c6eff8c57a2ae7141d9c7
Jonathan Esteban Roberts <jonathanroberts992@gmail.com>
2021-10-14 00:49:29 -0300

    npm update + autocomplete y buscador finalizado

--- Angular/05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
+++ Angular/05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,25 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BuscarComponent implements OnInit {
-
   termino: string = '';
   heroes: Heroe[] = [];
+  heroeSeleccionado: Heroe | undefined;
 
-  constructor(private heroesService: HeroesService) { }
+  constructor(private heroesService: HeroesService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  buscando(): void {
+    this.heroesService
+      .getSugerencias(this.termino.trim())
+      .subscribe((heroes) => (this.heroes = heroes));
   }
 
-  buscando(): void{
-    this.heroesService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent): void {
+    if(!event.option.value){
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroesService.getHeroePorId(heroe.id!).subscribe((heroe) => {
+      this.heroeSeleccionado = heroe;
+    });
   }
 }
