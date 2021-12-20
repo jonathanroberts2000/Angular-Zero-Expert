be936dbfb7f2bf1737e90d256ccc9baf9690202d
Jonathan Esteban Roberts <jonathanroberts992@gmail.com>
2021-12-19 22:49:18 -0300

    sidebar

--- Angular/06-formulariosApp/src/app/shared/shared.module.ts
+++ Angular/06-formulariosApp/src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
@@ -9,7 +10,8 @@ import { SidemenuComponent } from './sidemenu/sidemenu.component';
     SidemenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidemenuComponent
