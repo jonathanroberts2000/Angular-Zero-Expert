fae78b4b1c75a6191566fc6d560251e1a6a6678f
Jonathan Esteban Roberts <jonathanroberts992@gmail.com>
2022-01-16 17:39:02 -0300

    Switches reactivos

--- Angular/06-formulariosApp/src/app/reactive/switches/switches.component.ts
+++ Angular/06-formulariosApp/src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent implements OnInit{
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: false
+    });
+
+    this.miFormulario.valueChanges.subscribe(({condiciones, ...rest}) => {
+      //delete form.condiciones; ES DE JS
+      this.persona = rest;
+    });
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  guardar(): void{
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
   }
 
 }
