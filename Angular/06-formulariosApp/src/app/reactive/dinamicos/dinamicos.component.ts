519f2bbf2c0535fec63fed8ce8cd9fd7f787a3ec
Jonathan Esteban Roberts <jonathanroberts992@gmail.com>
2022-01-16 16:09:23 -0300

    Dinamicos reactive

--- Angular/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ Angular/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -11,13 +11,38 @@ export class DinamicosComponent {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.minLength(3)]],
-    favoritos: this.fb.array([], Validators.required)
+    favoritos: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ], Validators.required)
   });
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  get favoritosArr(): FormArray {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
 
   constructor(private fb: FormBuilder) { }
 
   campoEsValido(campo: string) {
     return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
+
+  agregarFavorito(): void{
+    if(this.nuevoFavorito.invalid){
+      return;
+    }
+
+    //AMBOS SON VALIDOS
+    //this.favoritosArr.push(new FormControl(this.nuevoFavorito.value, Validators.required));
+    this.favoritosArr.push(this.fb.control(this.nuevoFavorito.value, Validators.required));
+
+    this.nuevoFavorito.reset();
+  }
+
+  borrarFavorito(index: number): void{
+    this.favoritosArr.removeAt(index);
   }
 
   guardar(): void{
