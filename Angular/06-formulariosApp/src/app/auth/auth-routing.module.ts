b16df965825a19b6f315258cbeca79bd14fcc9fd
Jonathan Esteban Roberts <jonathanroberts992@gmail.com>
2022-01-16 23:29:46 -0300

    Estructura del registro

---
+++ Angular/06-formulariosApp/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,30 @@
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'registro',
+        component: RegistroComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'registro'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
