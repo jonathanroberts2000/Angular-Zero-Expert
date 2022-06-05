a1fbe0ffd46ed34d38ab62c36b93a8401fd489da
Jonathan Esteban Roberts <jonathanroberts992@gmail.com>
2022-06-05 18:52:38 -0300

    mapasApp

---
+++ Angular/09-mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-full-screen',
+  templateUrl: './full-screen.component.html',
+  styles: [
+    `
+      #mapa {
+        height: 100%;
+        width: 100%;
+      }
+    `
+  ]
+})
+export class FullScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-58.38157092508735, -34.603699530730474],
+      zoom: 18
+    });
+  }
+
+}
---
+++ Angular/09-mapasApp/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapboxToken: 'pk.eyJ1Ijoiam9uYXRoYW5yb2JlcnRzMjAwMCIsImEiOiJjbDNieGttNGkwM3dxM2xwYTBrZ2RoMXVzIn0.DwT63JxE3eNuNQE-3BZ4sg'
+};
